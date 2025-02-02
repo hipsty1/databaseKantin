02c6ed2d845ed6f51bb4f195a4683043a5c99ef7
Hendri Prasetyo <123240066@student.upnyk.ac.id>
2025-02-03 05:56:29 +0700

    Update kantin_upn .sql

--- kantin_upn .sql
+++ kantin_upn .sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 13 Nov 2024 pada 11.07
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `kantin_upn`
